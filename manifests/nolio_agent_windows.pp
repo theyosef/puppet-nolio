f7ad9978538266bb78650f2c61f9bc87b3b8d932
Choi, Sunghee <Sunghee.Choi@cdk.com>
2015-06-02 14:22:40 -0500

    change to if statement

--- manifests/nolio_agent_windows.pp
+++ manifests/nolio_agent_windows.pp
@@ -31,7 +31,12 @@ define nolio::nolio_agent_windows (
   $src_dir = "${temp_dir}\\puppet_nolio"
   $real_package_version = regsubst($package_version, '(\.)', '_', 'G')
   $real_package_name = "nolio_agent_windows_${real_package_version}.exe"
-  #$setNolioStagingRoot = 'setx NolioStagingRoot D:\\ /M'
+
+  if($nolioStagingRoot_dir == UNDEF){
+    $setNolioStagingRoot = 'setx NolioStagingRoot D:\\ /M'
+  }else{
+    $setNolioStagingRoot = "setx NolioStagingRoot ${nolioStagingRoot_dir} /M"
+  }
 
   Exec {
     path => "${::path}",
@@ -70,10 +75,6 @@ define nolio::nolio_agent_windows (
     require => Package[$package_name],
   }
 
-  case nolioStagingRoot_dir{
-    UNDEF   : { $setNolioStagingRoot = 'setx NolioStagingRoot D:\\ /M' }
-    default : { $setNolioStagingRoot = "setx NolioStagingRoot ${nolioStagingRoot_dir} /M" }
-  }->
   exec { 'Set NolioStaging Root':
     ensure => present,
     command => "${$setNolioStagingRoot}",
